d223e2545aa1deaec4e48898d1fbee6e2de430aa
Vikram Phatangare <vikram.phatangare@intelliswift.com>
2023-06-23 12:10:35 +0530

    added few test suites

---
+++ Test Suites/LUMA/13_BDD_Tests.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>13_BDD_Tests</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d7cb4685-906d-4604-a9b0-795e01219f58</testSuiteGuid>
+</TestSuiteEntity>
